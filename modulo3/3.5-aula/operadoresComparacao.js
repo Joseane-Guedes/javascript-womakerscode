f89d95177c8e8d6626e17b4d94cbe85747cdcbb8
Joseane-Guedes <joseane_23@hotmail.com>
2021-04-21 18:46:05 -0300

    Atualizacao aula operadores

---
+++ modulo3/3.5-aula/operadoresComparacao.js
@@ -0,0 +1,30 @@
+/**
+ * arquivo: operadoresComparacao.js
+ * descrição: explicação de como podemos usar operadores
+ * de coparação em JavaScript
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ *
+ */
+
+// Igual a == (true ou false)
+//console.log(10 == 10); // true  > verdadeiro
+//console.log(12 == 10); // false > falso
+
+// Diferente de !=
+/* console.log(10 != 10); // false
+console.log(10 != 9); // true */
+
+// Maior que >
+/* console.log(10 > 9); // true
+console.log(9 > 10); // false
+ */
+
+// Menor que < 
+/* console.log(9 < 10); // true
+console.log(10 < 9);  // false */
+
+// Valor e tipo igual a === o tres sinais de = = = ficaram assim por causa do Fira code
+console.log(10 === 10) // 10 ele é do tipo Number! (true)
+console.log(10 === "Joseane") // 10 -> Number | Joseane => String(false)
+
+
---
+++ modulo3/3.7-aula /operadoresLogicos.js
@@ -0,0 +1,17 @@
+/**
+ * arquivo: operadoresLogicos.js
+ * descrição: explicação de como podemos usar operadores
+ * lógicos em JavaScript
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ */
+
+// Operador And && (tendência a ser false)
+/* /*  */
+/* console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false  */
+
+/* // Operador Not !
+console.log(!true); // false
+console.log(!false) // true */ 
