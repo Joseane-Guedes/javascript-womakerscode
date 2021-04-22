691f26159ce77f27cfb3cb1398a35ad44b786238
Joseane-Guedes <joseane_23@hotmail.com>
2021-04-22 14:00:05 -0300

    mais atualizacoes mod5

--- modulo5/5.4-aula/arraysBasicos.js
+++ modulo5/5.4-aula/arraysBasicos.js
@@ -13,18 +13,52 @@
 Pop()  ==> Ele remove o ultimo elemento de um array;
 Push() ==> Ele tem como finalidade adicionar um novo elemento ao array e sempre no final deste array;
 shift() ==> Ele remove o primeiro elemento do array e desloca todos os outros elementos para o indice inferior.
-unshifit() ==> Ele adiciona um novo elemento ao array no inicio e remove os elementos mais antigos.
+unshifit() ==> Ele adiciona um novo elemento ao array no inicio
 
 */
 
 
-
-
-
-let eltetrodomesticos = [
+//==> Usos ==> Pop();
+/* let eltetrodomesticos = [
   'tv',
   'geladeira',
   'fogao',
   'microondas'
 ];
+console.log(eltetrodomesticos.pop()); */
+
+//==> Usos ==> Push();
+/*  let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+console.log('Sem o push..: ', eletrodomesticos);
+console.log(eletrodomesticos.push('Cafeteira'));
+console.log('Com o push...: ',  eletrodomesticos);  */
+
+//==> Usos ==> Shift();
+
+/* let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+console.log('Sem o shift..: ', eletrodomesticos);
+console.log(eletrodomesticos.shift());
+console.log('Com o shift...: ',  eletrodomesticos);  */
+
+// ==> unshift();
+let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+
+console.log('Sem o unshift..: ', eletrodomesticos);
+console.log(eletrodomesticos.unshift('Batedeira'));
+console.log('Com o unshift...: ',  eletrodomesticos);
 
