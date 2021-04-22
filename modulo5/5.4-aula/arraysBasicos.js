793a69e0dde10bed0e80e18c332a72d00618c772
Joseane-Guedes <joseane_23@hotmail.com>
2021-04-22 13:15:32 -0300

    atualizacao  mod5

---
+++ modulo5/5.4-aula/arraysBasicos.js
@@ -0,0 +1,30 @@
+/**
+ * arquivo: arraysBasicos.js
+ * descrição: explicação de como podemos usar Arrays em JavaScript
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ */
+
+// Lista de Objetos!
+//let nome_array = [item1, item2, item3 ...];
+//Uma array de eletrodomesticos
+
+/* Metodos básicos de arrays
+
+Pop()  ==> Ele remove o ultimo elemento de um array;
+Push() ==> Ele tem como finalidade adicionar um novo elemento ao array e sempre no final deste array;
+shift() ==> Ele remove o primeiro elemento do array e desloca todos os outros elementos para o indice inferior.
+unshifit() ==> Ele adiciona um novo elemento ao array no inicio e remove os elementos mais antigos.
+
+*/
+
+
+
+
+
+let eltetrodomesticos = [
+  'tv',
+  'geladeira',
+  'fogao',
+  'microondas'
+];
+
